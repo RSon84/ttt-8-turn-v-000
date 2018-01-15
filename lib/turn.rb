32e7fa4da005574015b1a62ca15c722c323c5507
Devin Wilson <craigvin84@yahoo.com>
2018-01-15 03:07:00 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
+
+ def move(board, position_selection, current_player = "X")
+   board[position_selection] = current_player
+ end
