ebdcf0be604bfbfd0d76f84c680d912b39adaa56
Devin Wilson <craigvin84@yahoo.com>
2018-01-15 03:16:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
 
+ def input_to_index(user_input)
+  user_input.to_i - 1
+ end
+
  def move(board, position_selection, current_player = "X")
    board[position_selection] = current_player
  end
@@ -25,5 +29,3 @@ def display_board(board)
       return false
   end
 end
-
- 
