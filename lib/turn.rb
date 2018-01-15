e866e550aefa8e8d2aa9cfcdae536249aba7240c
Devin Wilson <craigvin84@yahoo.com>
2018-01-15 05:19:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-
 def display_board(board)
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
@@ -7,38 +6,29 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
 
- def input_to_index(user_input)
-  user_input.to_i - 1
- end
-
- def move(board, position_selection, current_player = "X")
-   board[position_selection] = current_player
- end
-
- def position_taken?(board, index)
-   if board[index] != ("X") && board[index] != ("O")
-     return false
-   elsif board[index] == ("X") || board[index] == ("O")
-     return true
+ def valid_move?(board, position)
+   if position.to_i.between?(1,9)
+     if !position_taken?(board, position.to_i-1)
+      true
+     end
    end
  end
 
- def valid_move?(board, index)
-  if index.between?(0, 8) && !(position_taken?(board, index))
-    return true
-  else !index.between?(0, 8) || (position_taken?(board, index))
-      return false
-  end
-end
+ def turn(board)
+   puts "Please enter 1-9:"
+   input = gets.strip
+   if valid_move?(board, input)
+     move(board, input)
+   else
+     turn(board)
+   end
+   display_board(board)
+ end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  input_to_index(user_input)
-  until input_to_index(user_input) == valid_move?(board, index)
-  if input_to_index(user_input) == valid_move?(board, index)
-    display_board(board)
-    move(board, user_input, current_player = "X")
-    end
-  end
-end
+ def position_taken?(board, location)
+   board[location] != " "
+ end
+
+ def move(board, location, current_player = "X")
+   board[location.to_i-1] = current_player
+ end
